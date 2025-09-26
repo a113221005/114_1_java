2695bbb94154444a2a430df631b572f61300ae38
a113221005 <a113221005@mail.shu.edu.tw>
2025-09-26 14:52:03 +0800

    114-09-26

---
+++ 114-09-19/Review2_ClassBasic/src/AccountTest.java
@@ -0,0 +1,35 @@
+public class AccountTest {
+    public static void main(String[] args) {
+        // 建立一個帳戶物件，初始餘額為1000
+        Account account1 = new Account("A123", 1000.0);
+        Account account2 = new Account("B456", -2000.0);
+        ;
+
+        //System.out.println("帳戶號碼:" + account1.getAccountNumber());
+        //System.out.println("初始餘額:" + account1.getBalance());
+
+        System.out.printf("帳戶號碼: %s%n初始餘額: %.2f%n", account1.getAccountNumber(), account1.getBalance());
+        System.out.printf("帳戶號碼: %s%n初始餘額: %.2f%n", account2.getAccountNumber(), account1.getBalance());
+
+        account1.deposit(500.0);
+        System.out.printf("帳戶號碼: %s%n存款後餘額: %.2f%n", account1.getAccountNumber(), account1.getBalance());
+
+        account1.withdraw(1000.0);
+        System.out.printf("帳戶號碼: %s%n提款後餘額: %.2f%n", account1.getAccountNumber(), account1.getBalance());
+
+        try {
+            account1.deposit(-100.0);
+            System.out.printf("帳戶號碼: %s%n存款後餘額: %.2f%n", account1.getAccountNumber(), account1.getBalance());
+        } catch (IllegalArgumentException  e) {
+                System.out.println("存款失敗: " + e.getMessage());
+        }
+        try {
+            account1.withdraw(2000.0);
+            System.out.printf("帳戶號碼: %s%n提款後餘額: %.2f%n", account1.getAccountNumber(), account1.getBalance());
+        } catch (IllegalArgumentException  e) {
+                System.out.println("提款失敗: " + e.getMessage());
+        }
+
+    }
+
+}
